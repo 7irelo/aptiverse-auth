41921f37ea1d8661273d38c1662d0da71ae55509
7irelo <tirelo.eric@gmail.com>
2025-08-25 14:41:23 +0200

    feat(api): Add ServiceRegistration class for service registration
    Introduces the `ServiceRegistration` class in the `Aptiverse.Infrastructure` namespace. This class provides extension methods for `IServiceCollection` to register infrastructure services, user management services, and model services. It includes registrations for repositories and services related to admins, parents, students, teachers, users, and model tasks, along with necessary using directives.

---
+++ src/Aptiverse.Infrastructure/ServiceRegistartion/ServiceRegistration.cs
@@ -0,0 +1,46 @@
+﻿using Aptiverse.Application.Admins.Services;
+using Aptiverse.Application.AI.Services;
+using Aptiverse.Application.Parents.Services;
+using Aptiverse.Application.Students.Services;
+using Aptiverse.Application.Teachers.Services;
+using Aptiverse.Application.Users.Services;
+using Aptiverse.Domain.Interfaces;
+using Aptiverse.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aptiverse.Infrastructure.ServiceRegistartion
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddInfrastructureServices()
+                    .AddUserManagementServices()
+                    .AddModelServices();
+
+            return services;
+        }
+
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            return services;
+        }
+
+        public static IServiceCollection AddUserManagementServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAdminService, AdminService>();
+            services.AddScoped<IParentService, ParentService>();
+            services.AddScoped<IStudentService, StudentService>();
+            services.AddScoped<ITeacherService, TeacherService>();
+            services.AddScoped<IUserService, UserService>();
+            return services;
+        }
+
+        public static IServiceCollection AddModelServices(this IServiceCollection services)
+        {
+            services.AddScoped<IModelTaskService, ModelTaskService>();
+            return services;
+        }
+    }
+}
