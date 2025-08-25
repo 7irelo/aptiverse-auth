b8fb75c22f63bd42e8ae3baf60674b761e68f544
7irelo <tirelo.eric@gmail.com>
2025-08-25 14:32:28 +0200

    feat(students): Add Student DTO and service interface implementation
    Introduced a new `StudentDto` class for data transfer with properties for `Id` and `UserId`. Created the `IStudentService` interface with methods for managing student records. Implemented the `StudentService` class, which currently contains unimplemented methods for the defined interface.

---
+++ src/Aptiverse.Application/Students/Dtos/StudentDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace Aptiverse.Application.Students.Dtos
+{
+    [DataContract]
+    public class StudentDto
+    {
+        [DataMember(Name = "id")] public long Id { get; set; }
+        [DataMember(Name = "userId")] public string UserId { get; set; }
+    }
+}
