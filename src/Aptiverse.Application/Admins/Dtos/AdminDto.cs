128a016df5ed094eaae5512a79bfad89bc3d9ac5
7irelo <tirelo.eric@gmail.com>
2025-08-25 14:30:09 +0200

    feat(admins): Add AdminDto and AdminService implementation
    Introduce AdminDto for admin data transfer with Id and UserId properties. Implement AdminService class with method stubs for CRUD operations, adhering to the IAdminService interface. Update IAdminService to define the contract for admin-related operations.

---
+++ src/Aptiverse.Application/Admins/Dtos/AdminDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace Aptiverse.Application.Admins.Dtos
+{
+    [DataContract]
+    public class AdminDto
+    {
+        [DataMember(Name = "id")] public long Id { get; set; }
+        [DataMember(Name = "userId")] public string UserId { get; set; }
+    }
+}
---
+++ src/Aptiverse.Application/Admins/Services/AdminService.cs
@@ -0,0 +1,32 @@
+﻿using Aptiverse.Application.Admins.Dtos;
+
+namespace Aptiverse.Application.Admins.Services
+{
+    public class AdminService : IAdminService
+    {
+        public Task<AdminDto> CreateAdminAsync(AdminDto adminDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAdminAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AdminDto>> GetManyAdminsAsync(string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AdminDto> GetOneAdminAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AdminDto> UpdateAdminAsync(long id, AdminDto adminDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
