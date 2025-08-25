dd49e4bb0e84ffcf454f8d20063b0a514a042958
7irelo <tirelo.eric@gmail.com>
2025-08-25 14:32:28 +0200

    feat(students): Add Student DTO and service interface implementation
    Introduced a new `StudentDto` class for data transfer with properties for `Id` and `UserId`. Created the `IStudentService` interface with methods for managing student records. Implemented the `StudentService` class, which currently contains unimplemented methods for the defined interface.

---
+++ src/Aptiverse.Application/Students/Services/StudentService.cs
@@ -0,0 +1,32 @@
+﻿using Aptiverse.Application.Students.Dtos;
+
+namespace Aptiverse.Application.Students.Services
+{
+    public class StudentService : IStudentService
+    {
+        public Task<StudentDto> CreateStudentAsync(StudentDto studentDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteStudentAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<StudentDto>> GetManyStudentAsync(string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<StudentDto> GetOneStudentAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<StudentDto> UpdateStudentAsync(long id, StudentDto studentDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
