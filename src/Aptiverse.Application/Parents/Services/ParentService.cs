4e6b051c6d4f0e4d57fe8ed19cde3eedf8d4122e
7irelo <tirelo.eric@gmail.com>
2025-08-25 14:32:08 +0200

    feat(parents): Add ParentDto and IParentService implementation
    Introduce ParentDto for parent data representation.
    Create IParentService interface with methods for CRUD operations.
    Implement ParentService class with method signatures, pending logic.

---
+++ src/Aptiverse.Application/Parents/Services/ParentService.cs
@@ -0,0 +1,32 @@
+﻿using Aptiverse.Application.Parents.Dtos;
+
+namespace Aptiverse.Application.Parents.Services
+{
+    public class ParentService : IParentService
+    {
+        public Task<ParentDto> CreateParentAsync(ParentDto parentDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteParentAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ParentDto>> GetManyParentAsync(string filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ParentDto> GetOneParentAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ParentDto> UpdateParentAsync(long id, ParentDto parentDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
