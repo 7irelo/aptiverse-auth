29f437c77b271079ddf2f2304430eaf0ad11b684
7irelo <tirelo.eric@gmail.com>
2025-08-25 14:37:38 +0200

    feat(persistance): Add admin, student, parent, and teacher models
    Introduce new junction classes for Admin, StudentAdmin, StudentParent, StudentTeacher, Parent, Student, and Teacher to establish relationships in the Aptiverse domain. Update ApplicationUser to inherit from IdentityUser, adding user-related properties. Remove legacy User, UserCategory, UserType, and Email classes for a streamlined user management approach.

---
+++ src/Aptiverse.Domain/Models/Admins/Admin.cs
@@ -0,0 +1,13 @@
+﻿using Aptiverse.Domain.Models.Junctions;
+using Aptiverse.Domain.Models.Users;
+
+namespace Aptiverse.Domain.Models.Admins
+{
+    public class Admin
+    {
+        public long Id { get; set; }
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public virtual ICollection<StudentAdmin> StudentAdmins { get; set; } = new List<StudentAdmin>();
+    }
+}
---
+++ src/Aptiverse.Domain/Models/Users/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Aptiverse.Domain.Models.Users
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
